af37386e9e6d7d052e3c35e2e25839ea26ad1ca2
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-11 14:56:13 +0100

    [add] bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
+      isLoaded: false,
       emails:[],
       nEmails: 10,
       apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
@@ -10,13 +11,14 @@ createApp({
   },
   methods:{
     getApi(){
+      this.isLoaded = false;
       for (let i = 0; i < this.nEmails; i++) {
         axios.get(this.apiUrl)
         .then(result => {
         this.emails.push(result.data.response);
-        console.log(this.emails);
       })
       }
+      this.isLoaded= true;
     }
   },
   mounted(){
